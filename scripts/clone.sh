251b8d4a2ce736b6b09464bb5d2a07797826a480
barakiva <barakiva1@gmail.com>
2026-04-20 09:15:09 +0300

    fix: fixed linting error

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-set -eux
+#!/bin/bash
+set -euo pipefail
 
 GIT_TREE="$(basename "${APP_GH_REF}")"
 git clone "${UPSTREAM_URL}" .
 git checkout "${GIT_TREE}"
 if [ "${APP_GH_ADD_SHA}" = "true" ]; then
-    git describe --tags --long > config/custom-version.txt
+  git describe --tags --long >config/custom-version.txt
 fi
 rm -rf .git
